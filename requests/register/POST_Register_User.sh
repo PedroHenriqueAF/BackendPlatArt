4c9cd3b1013561294f8fd689741cba9bfd79e346
luccasp <luccaspino@icloud.com>
2025-06-11 15:39:05 -0300

    modified:   requests/login/POST_Login_User.sh 	modified:   requests/register/POST_Register_User.sh 	modified:   src/controller/user.controller.js 	modified:   src/models/User.js 	modified:   src/routes/user.route.js 	modified:   src/services/user.service.js

--- requests/register/POST_Register_User.sh
+++ requests/register/POST_Register_User.sh
@@ -1,10 +1,13 @@
+
 curl --request POST \
   --url 'http://localhost:3000/users/register' \
   --header 'Content-Type: application/json' \
   --data '{
     "id": 1,
     "nome": "novo_usuario",
-    "tipo": "cliente"
+    "tipo": "cliente",
+    "password": "senha123"
   }'
+
   
 // source ./requests/register/POST_Register_User.sh
